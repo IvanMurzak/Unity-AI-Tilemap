981f2d917b857c5259348bdc31d426a538e90fa2
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-06-09 23:38:55 -0700

    AI Game Developer update

--- Installer/Assets/com.IvanMurzak/AI Tilemap Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI Tilemap Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.Tilemap.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.tilemap";
-        public const string Version = "1.0.2";
+        public const string Version = "1.0.3";
 
         static Installer()
         {
